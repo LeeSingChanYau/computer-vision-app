dc9509af630149a5d5fc50797f369471e36eb899
Obeda Velonjatovo <velonjatovoa@gmail.com>
2024-03-31 16:07:37 -0700

    Make scroll up the text field automatically when user types so that the keyboard won't hide the text field

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Signup({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function onChangeEmail(t) {
     setEmail(t);
@@ -16,12 +17,12 @@ export default function Signup({ navigation }) {
 
   function handleSignUp() {
     axios
-      .post('http://10.0.0.67:8080/signup', {
+      .post("http://10.0.0.67:8080/signup", {
         email: email,
         password: password,
       })
       .then(function (response) {
-        navigation.navigate('Verify', {
+        navigation.navigate("Verify", {
           email: email,
         });
       })
@@ -36,62 +37,65 @@ export default function Signup({ navigation }) {
   }, [email, password]);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logo}>
-        <Image
-          source={require('../assets/AJ1.png')}
-          style={{ width: 200, height: 200 }}
-        />
-        <Text style={styles.text}>What are those?</Text>
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image
+            source={require("../assets/AJ1.png")}
+            style={{ width: 200, height: 200 }}
+          />
+          <Text style={styles.text}>What are those?</Text>
+        </View>
+        <View style={styles.subContainer}>
+          <Text style={styles.text}>Signup</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            placeholder="Email"
+            onChangeText={onChangeEmail}
+            value={email}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={onChangePassword}
+            value={password}
+          />
+          <Button
+            title="Sign Up"
+            onPress={handleSignUp}
+            color={"rgb(255, 45, 85)"}
+          />
+          <Button
+            title="Already have an account? Log In"
+            onPress={() => navigation.navigate("Login")}
+            color={"rgb(255, 45, 85)"}
+          />
+        </View>
       </View>
-      <View style={styles.subContainer}>
-        <Text style={styles.text}>Signup</Text>
-        <TextInput
-          style={styles.input}
-          autoCapitalize="none"
-          placeholder="Email"
-          onChangeText={onChangeEmail}
-          value={email}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={true}
-          onChangeText={onChangePassword}
-          value={password}
-        />
-        <Button
-          title="Sign Up"
-          onPress={handleSignUp}
-          color={'rgb(255, 45, 85)'}
-        />
-        <Button
-          title="Already have an account? Log In"
-          onPress={() => navigation.navigate('Login')}
-          color={'rgb(255, 45, 85)'}
-        />
-      </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   subContainer: {
     flex: 3,
-    alignItems: 'center',
+    alignItems: "center",
   },
   text: {
-    color: 'rgb(255, 45, 85)',
+    color: "rgb(255, 45, 85)",
   },
   logo: {
     flex: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 100,
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     height: 40,
@@ -99,8 +103,8 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     borderRadius: 5,
-    borderColor: 'white',
-    backgroundColor: 'white',
+    borderColor: "white",
+    backgroundColor: "white",
     padding: 10,
   },
 });
