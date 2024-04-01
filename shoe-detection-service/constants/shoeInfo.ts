ffc2478c5ebf846842dd78a9f0bbb22964f0b8c0
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2024-03-31 18:07:59 -0700

    Added shoeInfo.ts

---
+++ shoe-detection-service/constants/shoeInfo.ts
@@ -0,0 +1,55 @@
+export const shoeInfo = [
+  {
+    name: 'Air Jordan 1',
+    releaseYear: '1985',
+    description: `The Air Jordan 1, often referred to simply as the "Jordan 1," 
+    is a basketball shoe that has transcended its original purpose to become a cultural icon in the world of fashion and footwear. 
+    This sneaker's history is deeply intertwined with the rise of the legendary Michael Jordan, 
+    who would go on to become one of the greatest basketball players of all time. Designed by Peter Moore and crafted by Nike, 
+    the Air Jordan 1 introduced a revolutionary concept in both performance and style when it was first released in 1985, 
+    forever changing the landscape of sneaker design and fashion.`,
+  },
+  {
+    name: 'Air Jordan 2',
+    releaseYear: '1986',
+    description: `The Air Jordan II featured a full-length Air-Sole unit and a polyurethane midsole. 
+    The shoe bore a similar silhouette to the Nike Air Python that would release the following year. 
+    It featured a faux lizard skin and swooping lines that resembled the detailing of a sports car. 
+    This design aesthetic would factor into the Air Jordan line later in the series. 
+    The Air Jordan II was the first Jordan not to have the Nike swoosh on the upper, 
+    though "Nike" was stitched across the heel counter. 
+    Michael Jordan wore the Air Jordan II for a shortened 18 games in the 1986\u201387 season due to a broken foot.`,
+  },
+  {
+    name: 'Air Jordan 3',
+    releaseYear: '1987',
+    description: `The Air Jordan 3, a pivotal milestone in the Jordan brand's history, 
+    has solidified its status as one of the most iconic sneakers in the world. 
+    Designed by Tinker Hatfield and introduced in 1988, 
+    the Air Jordan 3 marked a turning point not only for Michael Jordan's signature line but also for the entire sneaker industry, 
+    influencing both athletic performance and streetwear fashion.
+    The Air Jordan 3 introduced several groundbreaking features, most notably the visible Air unit in the sole, 
+    which not only improved cushioning but also set a trend for future Nike sneakers.`,
+  },
+  {
+    name: 'Air Jordan 4',
+    releaseYear: '1989',
+    description: `The Air Jordan 4, a celebrated creation in the legendary Air Jordan line by Nike, 
+    has etched its place in the annals of sneaker history. Designed by the renowned Tinker Hatfield, 
+    this silhouette made its debut in 1989, 
+    introducing a host of innovative features that have since influenced both athletic performance and fashion.
+    This model was the first in the Air Jordan line to incorporate visible Air cushioning in both the forefoot and heel, 
+    enhancing comfort and responsiveness for basketball players. The shoe also featured a plastic wing eyelet design on the sides, 
+    providing enhanced lockdown and stability.`,
+  },
+  {
+    name: 'Air Jordan 5',
+    releaseYear: '1990',
+    description: `Tinker Hatfield's design of the Air Jordan V was inspired by a fighter jet called the Supermarine Spitfire Mk VIII, 
+    a World War II era British fighter jet.  Tinker used “teeth” on the midsole to give the AJ V an aggressive look, 
+    making the parallel between the way Jordan played and the way fighter planes maneuvered and attacked the battlefield.
+    The Air Jordan V had many firsts. It was the first Air Jordan to use a clear (or translucent) outsole, 
+    it was the first shoe to make use of molded foam in the shoe's ankle collar, 
+    it was also the first to be included with a unique set of lace locks. `,
+  },
+];
